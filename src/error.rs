4da41a481e70c006ef80794a05ec1f2c83ec69a7
sunwenming <you@example.com>
2022-08-03 19:47:37 +0800

    error

---
+++ src/error.rs
@@ -0,0 +1,58 @@
+use thiserror::Error;
+// 1. 实现 Debug 与 std::fmt::Display
+// 2. 实现 std::error::Error 特质
+//  - 没有子 Error 只需要 `impl std::error::Error for <YourError> {}`
+//  - 有子 Error 需要实现 source() 返回 Some(<YourChildError>)
+#[derive(Error, Debug)]
+pub enum CustomError {
+    #[error("number `{0}` is gt 200")]
+    NumberGt200Error(usize),
+    #[error("not found")]
+    NotFoundError,
+    #[error("not found")]
+    Network(NetworkError),
+    #[error("{0}")]
+    IOError(#[from] std::io::Error),
+}
+
+#[derive(Error, Debug)]
+pub enum NetworkError {
+    #[error("time out")]
+    TimeOut,
+}
+
+// impl From<std::io::Error> for CustomError {
+//     fn from(e: std::io::Error) -> Self {
+//         Self::IOError(e)
+//     }
+// }
+
+// impl Error for CustomError {
+//     fn source(&self) -> Option<&(dyn Error + 'static)> {
+//         match self {
+//             CustomError::Network(e) => Some(e),
+//             _ => None,
+//         }
+//     }
+// }
+
+// impl Display for CustomError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             CustomError::NumberGt200Error(number) => write!(f, "number is {}, 期待小于200", number),
+//             CustomError::NotFoundError => write!(f, "not found"),
+//             CustomError::Network(sub) => write!(f, "{}", sub),
+//             CustomError::IOError(sub) => write!(f, "{}", sub),
+//         }
+//     }
+// }
+
+// impl Error for NetworkError {}
+
+// impl Display for NetworkError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             NetworkError::TimeOut => write!(f, "time out"),
+//         }
+//     }
+// }
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+
+use anyhow::{Context, Result};
+use error::{CustomError, NetworkError};
+
+mod error;
+
+fn get_number_gt_200() -> Result<(), CustomError> {
+    Err(CustomError::NumberGt200Error(300))
+}
+fn get_network_error() -> Result<(), CustomError> {
+    Err(CustomError::Network(NetworkError::TimeOut))
+}
+
+fn get_io_error(path: String) -> Result<(), CustomError> {
+    match File::open(path) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(CustomError::from(e)),
+    }
+}
+
+fn main() -> Result<()> {
+    // match get_number_gt_200() {
+    //     Ok(_) => (),
+    //     Err(e) => println!("{}", e),
+    // }
+    // match get_network_error() {
+    //     Ok(_) => (),
+    //     Err(e) => println!("{}", e),
+    // }
+    let path = "a.txt".to_string();
+    get_io_error(path.clone()).with_context(|| format!("找不到此文件,{}", path))?;
+    Ok(())
+}
